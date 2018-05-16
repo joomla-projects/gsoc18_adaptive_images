c8bba8601842cbb3eeee6bbed226653dfd72ec9e
Geetanshu <mathur.geetanshu887@gmail.com>
2018-05-16 10:20:08 +0530

    plugin added

---
+++ plugins/media-action/smartcrop/smartcrop.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  Media-Action.smart-crop
+ *
+ * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\HTML\HTMLHelper;
+
+/**
+ * Media Manager Smart Crop Action
+ *
+ * @since  4.0.0
+ */
+class PlgMediaActionSmartCrop extends \Joomla\Component\Media\Administrator\Plugin\MediaActionPlugin
+{
+    /**
+     * Load the javascript files of the plugin.
+     *
+     * @return  void
+     *
+     * @since   4.0.0
+     */
+    protected function loadJs()
+    {
+        parent::loadJs();
+
+        HTMLHelper::_('script', 'plg_media-action_smartcrop/responsifyjs/responsify.js', array('version' => 'auto', 'relative' => true));
+    }
+
+    /**
+     * Load the CSS files of the plugin.
+     *
+     * @return  void
+     *
+     * @since   4.0.0
+     */
+    protected function loadCss()
+    {
+        parent::loadCss();
+    }
+}
