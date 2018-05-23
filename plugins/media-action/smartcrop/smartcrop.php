5c44ad3b94cd6a841a4582c03836b79496f767b5
Geetanshu <mathur.geetanshu887@gmail.com>
2018-05-23 17:33:12 +0530

    testing

--- plugins/media-action/smartcrop/smartcrop.php
+++ plugins/media-action/smartcrop/smartcrop.php
@@ -9,6 +9,7 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Form\Form;
 use Joomla\CMS\HTML\HTMLHelper;
 
 /**
@@ -18,42 +19,56 @@ use Joomla\CMS\HTML\HTMLHelper;
  */
 class PlgMediaActionSmartCrop extends \Joomla\Component\Media\Administrator\Plugin\MediaActionPlugin
 {
-	/**
-	 * Load the language file on instantiation (for Joomla! 3.X only)
-	 *
-	 * @var    boolean
-	 * @since  3.3
-	 */
-	protected $autoloadLanguage = true;
-
-	/**
-	 * Load the javascript files of the plugin.
-	 *
-	 * @return  void
-	 *
-	 * @since   4.0.0
-	 */
-	protected function loadJs()
+	public function __construct()
 	{
-		parent::loadJs();
-
-		/*
-		* This Library is to be used on the client side
-		*/
-
-		// HTMLHelper::_('script', 'plg_media-action_smartcrop/responsifyjs/responsify.js', array('version' => 'auto', 'relative' => true));
+		echo "2";
 	}
 
-	/**
-	 * Load the CSS files of the plugin.
-	 *
-	 * @return  void
-	 *
-	 * @since   4.0.0
-	 */
-	protected function loadCss()
+	public function onContentPrepare($context, &$row, $params, $page = 0)
 	{
+		echo "1";
+	}
+
+	/*
+	* @Override the parent function onContentPrepareForm
+	*
+	* @param   Form       $form  The form
+	* @param   \stdClass  $data  The data
+	*
+	* @return  void
+	*
+	* @since   4.0.0
+	*/
+
+	public function onContentPrepareForm(Form $form, $data)
+	{
+		// Check if it is the right form
+		if ($form->getName() != 'com_media.file')
+		{
+			return;
+		}
+
+		// Loading the JS file from the parent class
+		parent::loadJs();
+
+		// Loading the CSS file from the parent class
 		parent::loadCss();
+
+		// The file with the params for the edit view
+		$paramsFile = JPATH_PLUGINS . '/media-action/' . $this->_name . '/form/' . $this->_name . '.xml';
+
+		// When the file exists, load it into the form
+		if (file_exists($paramsFile))
+		{
+			$form->loadFile($paramsFile);
+		}
+		
+		// $this->saveDataFocusPoint($form->getValue("jform_smartcrop_quality","","hello"));
+	}
+
+	public function onContentPrepareData($context,$data){
+		print_r($context);
+
 	}
 
 	/*
@@ -67,4 +82,23 @@ class PlgMediaActionSmartCrop extends \Joomla\Component\Media\Administrator\Plug
 		'data-focus-bottom' => '',
 		'data-focus-right' => ''
 	);
+
+	/*
+	* Saveing the Data Focus point into the JSON file.
+	*/
+
+	public function saveDataFocusPoint($form){
+		//print_r($form);
+	}
+
+	/*
+	* Replaceing img tags with the tags with the data-focus points.
+	*/
+
+	public function replaceImg(&$article){
+		$images = array();
+		preg_match_all('/{img src=(.*?)}/is', $article->text, $images);
+		echo "HI...";
+		print_r($images);
+	}
 }
