1157b75fd8a515b19c5922b6ddd1c36198b1eb94
Geetanshu <mathur.geetanshu887@gmail.com>
2018-05-24 22:01:51 +0530

    adaptive image content plugin

---
+++ plugins/content/adaptiveimage/adaptiveimage.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  Content.adaptiveimage
+ *
+ * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Plugin\CMSPlugin;
+
+/**
+ * Adaptive Image Plugin
+ *
+ * @since  4.0
+ */
+class PlgContentAdaptiveImage extends CMSPlugin
+{
+
+	/**
+	 * Load the language file on instantiation.
+	 *
+	 * @var    boolean
+	 *
+	 * @since  4.0.0
+	 */
+	protected $autoloadLanguage = true;
+
+	/**
+	 * Plugin that inserts focus points into the image.
+	 *
+	 * @param   string   $context  The context of the content being passed to the plugin.
+	 * @param   mixed    &$row     An object with a "text" property.
+	 * @param   mixed    &$params  Additional parameters.
+	 * @param   integer  $page     Optional page number. Unused. Defaults to zero.
+	 *
+	 * @return  boolean	True on success.
+	 */
+	public function onContentPrepare($context, &$row, &$params, $page = 0)
+	{
+		// Don't run this plugin when the content is being indexed
+		if ($context === 'com_finder.indexer')
+		{
+			return true;
+		}
+
+		if (is_object($row))
+		{
+			return $this->insertFocus($row->text, $params);
+		}
+
+		return $this->insertFocus($row, $params);
+	}
+
+	/**
+	 * Inserts focus points into the image.
+	 *
+	 * @param   string  &$text    HTML string.
+	 * @param   mixed   &$params  Additional parameters. Parameter "mode" (integer, default 1)
+	 *                             replaces addresses with "mailto:" links if nonzero.
+	 *
+	 * @return  boolean  True on success.
+	 */
+	protected function insertFocus(&$text, &$params)
+	{
+		
+		$searchImage = '(<img[^>]+>)';
+
+		preg_match_all($searchImage, $text, $images);
+
+		foreach($images[0] as $key => $image)
+		{
+			$focus = "data-focus-left=\"\" data-focus-top=\"\" data-focus-right=\"\" data-focus-bottom=\"\" />";
+
+			$newTag = str_replace("/>", $focus, $image);
+
+			$text = str_replace($image, $newTag, $text);
+		}
+		
+		return true;
+	}
+}
