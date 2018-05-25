94e67758fac22cc63e6ef5fbbead40d05f86dc86
Geetanshu <mathur.geetanshu887@gmail.com>
2018-05-25 10:07:17 +0530

    cleanup

--- media/plg_media-action_smartcrop/js/smartcrop.js
+++ media/plg_media-action_smartcrop/js/smartcrop.js
@@ -28,16 +28,16 @@ Joomla.MediaManager.Edit = Joomla.MediaManager.Edit || {};
 
 				// Top, Bottom, Left, right are the data focus points
                 var canvas_data = this.cropper.getCropBoxData();
-                var top = canvas_data.top / image.naturalHeight;
-                var bottom = (canvas_data.height + canvas_data.top) / image.naturalHeight;
-                var left = canvas_data.left / image.naturalWidth;
-				var right = (canvas_data.width + canvas_data.left) / image.naturalWidth;
+                Joomla.MediaManager.Edit.smartcrop.cropper.top = (canvas_data.top / image.naturalHeight).toFixed(2);
+                Joomla.MediaManager.Edit.smartcrop.cropper.bottom = ((canvas_data.height + canvas_data.top) / image.naturalHeight).toFixed(2);
+                Joomla.MediaManager.Edit.smartcrop.cropper.left = (canvas_data.left / image.naturalWidth).toFixed(2);
+				Joomla.MediaManager.Edit.smartcrop.cropper.right = ((canvas_data.width + canvas_data.left) / image.naturalWidth).toFixed(2);
 				
 				// Setting the computed focus point into the input fields
-				document.getElementById('jform_data_focus_top').value = top.toFixed(2);
-				document.getElementById('jform_data_focus_bottom').value = bottom.toFixed(2);
-				document.getElementById('jform_data_focus_left').value = left.toFixed(2);
-				document.getElementById('jform_data_focus_right').value = right.toFixed(2);
+				document.getElementById('jform_data_focus_top').value = Joomla.MediaManager.Edit.smartcrop.cropper.top;
+				document.getElementById('jform_data_focus_bottom').value = Joomla.MediaManager.Edit.smartcrop.cropper.bottom;
+				document.getElementById('jform_data_focus_left').value = Joomla.MediaManager.Edit.smartcrop.cropper.left;
+				document.getElementById('jform_data_focus_right').value = Joomla.MediaManager.Edit.smartcrop.cropper.right;
 
 				// Manageing image extension 
 				var format = Joomla.MediaManager.Edit.original.extension === 'jpg' ? 'jpeg' : Joomla.MediaManager.Edit.original.extension;
@@ -48,7 +48,7 @@ Joomla.MediaManager.Edit = Joomla.MediaManager.Edit || {};
 				// Notify the app that a change has been made
 				window.dispatchEvent(new Event('mediaManager.history.point'));
 			}
-        });
+		});
     }
 
     // Register the Events
--- plugins/media-action/smartcrop/smartcrop.php
+++ plugins/media-action/smartcrop/smartcrop.php
@@ -9,9 +9,6 @@
 
 defined('_JEXEC') or die;
 
-use Joomla\CMS\Form\Form;
-use Joomla\CMS\HTML\HTMLHelper;
-
 /**
  * Media Manager Smart Crop Action
  *
@@ -19,86 +16,27 @@ use Joomla\CMS\HTML\HTMLHelper;
  */
 class PlgMediaActionSmartCrop extends \Joomla\Component\Media\Administrator\Plugin\MediaActionPlugin
 {
-	public function __construct()
+	/**
+	 * Load the javascript files of the plugin.
+	 *
+	 * @return  void
+	 *
+	 * @since   4.0.0
+	 */
+	protected function loadJs()
 	{
-		echo "2";
-	}
-
-	public function onContentPrepare($context, &$row, $params, $page = 0)
-	{
-		echo "1";
-	}
-
-	/*
-	* @Override the parent function onContentPrepareForm
-	*
-	* @param   Form       $form  The form
-	* @param   \stdClass  $data  The data
-	*
-	* @return  void
-	*
-	* @since   4.0.0
-	*/
-
-	public function onContentPrepareForm(Form $form, $data)
-	{
-		// Check if it is the right form
-		if ($form->getName() != 'com_media.file')
-		{
-			return;
-		}
-
-		// Loading the JS file from the parent class
 		parent::loadJs();
+	}
 
-		// Loading the CSS file from the parent class
+	/**
+	 * Load the CSS files of the plugin.
+	 *
+	 * @return  void
+	 *
+	 * @since   4.0.0
+	 */
+	protected function loadCss()
+	{
 		parent::loadCss();
-
-		// The file with the params for the edit view
-		$paramsFile = JPATH_PLUGINS . '/media-action/' . $this->_name . '/form/' . $this->_name . '.xml';
-
-		// When the file exists, load it into the form
-		if (file_exists($paramsFile))
-		{
-			$form->loadFile($paramsFile);
-		}
-		
-		// $this->saveDataFocusPoint($form->getValue("jform_smartcrop_quality","","hello"));
-	}
-
-	public function onContentPrepareData($context,$data){
-		print_r($context);
-
-	}
-
-	/*
-	* Defineing the variables
-	*
-	* @var array
-	*/
-	protected $data_focus = array(
-		'data-focus-top' => '',
-		'data-focus-left' => '',
-		'data-focus-bottom' => '',
-		'data-focus-right' => ''
-	);
-
-	/*
-	* Saveing the Data Focus point into the JSON file.
-	*/
-
-	public function saveDataFocusPoint($form){
-		//print_r($form);
-	}
-
-	/*
-	* Replaceing img tags with the tags with the data-focus points.
-	*/
-
-	public function replaceImg(&$article){
-		$images = array();
-		preg_match_all('/{img src=(.*?)}/is', $article->text, $images);
-		echo "HI...";
-		print_r($images);
 	}
 }
