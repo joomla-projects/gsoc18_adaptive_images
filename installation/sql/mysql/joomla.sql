e6ac7515d7aa2c175eeacc0d396a71e4d906e293
Geetanshu Mathur <mathur.geetanshu887@gmail.com>
2018-06-24 15:17:25 +0530

    Adding plugin to the installation script

--- installation/sql/mysql/joomla.sql
+++ installation/sql/mysql/joomla.sql
@@ -642,6 +642,8 @@ INSERT INTO `#__extensions` (`extension_id`, `package_id`, `name`, `type`, `elem
 (485, 0, 'plg_media-action_rotate', 'plugin', 'rotate', 'media-action', 0, 1, 1, 0, '', '{}', 0, '0000-00-00 00:00:00', 0, 0, ''),
 (486, 0, 'plg_installer_webinstaller', 'plugin', 'webinstaller', 'installer', 0, 1, 1, 0, '', '{"tab_position":"1"}', 0, '0000-00-00 00:00:00', 0, 0, ''),
 (487, 0, 'plg_system_httpheaders', 'plugin', 'httpheaders', 'system', 0, 1, 1, 0, '', '{}', 0, '0000-00-00 00:00:00', 0, 0, ''),
+(490, 0, 'plg_media-action_smartcrop', 'plugin', 'smartcrop', 'media-action', 0, 1, 1, 0, '', '{}', 0, '0000-00-00 00:00:00', 1, 0, ''),
+(491, 0, 'plg_content_adaptiveimage', 'plugin', 'adaptiveimage', 'content', 0, 1, 1, 0, '', '{}', 0, '0000-00-00 00:00:00', 0, 0, ''),
 (509, 0, 'atum', 'template', 'atum', '', 1, 1, 1, 0, '', '', 0, '0000-00-00 00:00:00', 0, 0, ''),
 (510, 0, 'cassiopeia', 'template', 'cassiopeia', '', 0, 1, 1, 0, '', '{"logoFile":"","fluidContainer":"0","sidebarLeftWidth":"3","sidebarRightWidth":"3"}', 0, '0000-00-00 00:00:00', 0, 0, ''),
 (600, 802, 'English (en-GB)', 'language', 'en-GB', '', 0, 1, 1, 1, '', '', 0, '0000-00-00 00:00:00', 0, 0, ''),
