9dace6a0aac19469c2581a423a915729ccac794c
Geetanshu Mathur <mathur.geetanshu887@gmail.com>
2018-07-14 21:21:28 +0530

    Fetching the parameters

--- plugins/media-action/smartcrop/smartcrop.php
+++ plugins/media-action/smartcrop/smartcrop.php
@@ -9,13 +9,62 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Form\Form;
+use Joomla\CMS\Plugin\CMSPlugin;
+
 /**
  * Media Manager Smart Crop Action
  *
  * @since  4.0.0
  */
-class PlgMediaActionSmartCrop extends \Joomla\Component\Media\Administrator\Plugin\MediaActionPlugin
+class PlgMediaActionSmartCrop extends CMSPlugin
 {
+	/**
+	 * Load the language file on instantiation.
+	 *
+	 * @var    boolean
+	 *
+	 * @since  4.0.0
+	 */
+	protected $autoloadLanguage = true;
+	/**
+	 * The form event. Load additional parameters when available into the field form.
+	 * Only when the type of the form is of interest.
+	 *
+	 * @param   Form       $form  The form
+	 * @param   \stdClass  $data  The data
+	 *
+	 * @return  void
+	 *
+	 * @since   4.0.0
+	 */
+	public function onContentPrepareForm(Form $form, $data)
+	{
+		// Check if it is the right form
+		if ($form->getName() != 'com_media.file')
+		{
+			return;
+		}
+		
+		// Fetch the parameters.
+		$parameter_object = $this->params->get('custom_width', false);
+		$Widths = [];
+		foreach ($parameter_object as $customWidth) 
+			$Widths[] = $customWidth->Widths;
+
+		$this->loadCss();
+		$this->loadJs();
+
+		// The file with the params for the edit view
+		$paramsFile = JPATH_PLUGINS . '/media-action/' . $this->_name . '/form/' . $this->_name . '.xml';
+
+		// When the file exists, load it into the form
+		if (file_exists($paramsFile))
+		{
+			$form->loadFile($paramsFile);
+		}
+	}
+
 	/**
 	 * Load the javascript files of the plugin.
 	 *
@@ -25,7 +74,11 @@ class PlgMediaActionSmartCrop extends \Joomla\Component\Media\Administrator\Plug
 	 */
 	protected function loadJs()
 	{
-		parent::loadJs();
+		\JHtml::_(
+		'script',
+			'plg_media-action_' . $this->_name . '/' . $this->_name . '.js',
+			array('version' => 'auto', 'relative' => true)
+		);
 	}
 
 	/**
@@ -37,6 +90,10 @@ class PlgMediaActionSmartCrop extends \Joomla\Component\Media\Administrator\Plug
 	 */
 	protected function loadCss()
 	{
-		parent::loadCss();
+		\JHtml::_(
+		'stylesheet',
+			'plg_media-action_' . $this->_name . '/' . $this->_name . '.css',
+			array('version' => 'auto', 'relative' => true)
+		);
 	}
 }
